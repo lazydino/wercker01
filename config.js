b7a65e793842eba8fc462672f8e48a6619f58ad5
Mateusz Derks <mateusz.derks@schibsted.pl>
2015-08-08 12:00:45 +0200

    Fix database connection

--- config.js
+++ config.js
@@ -9,13 +9,7 @@ config = {
 
         database: {
             client: 'postgres',
-            connection: {
-                host: process.env.POSTGRES_HOST,
-                user: process.env.POSTGRES_USER,
-                password: process.env.POSTGRES_PASSWORD,
-                database: process.env.POSTGRES_DATABASE,
-                port: '5432'
-            }
+            connection: process.env.DATABASE_URL
         },
 
         server: {
